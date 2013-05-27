c5c91fbaff3c3d62c84849e96af9c80077a13d7a
kk <fkfkbill@gmail.com>
2013-05-27 22:08:38 +0800

    created urls

--- sleepyhypnos/settings.py
+++ sleepyhypnos/settings.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Django settings for sleepyhypnos project.
 
 DEBUG = True
@@ -105,7 +106,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'sleepyhypnos.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
@@ -120,7 +121,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     'django.contrib.admin',
+    'grappelli',
+    'django.contrib.admin',
 )
 
 # A sample logging configuration. The only tangible logging
--- sleepyhypnos/urls.py
+++ sleepyhypnos/urls.py
@@ -1,8 +1,28 @@
+# -*- coding: utf-8 -*-
+
+
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
 
+from sleepyhypnos import views
+
 urlpatterns = patterns('',
-     url(r'^admin/', include(admin.site.urls)),
+	url(r'^grappelli/', include('grappelli.urls')),
+	url(r'^admin/', include(admin.site.urls)),
+	
+	#account management
+	url(r"^reg/?$",views.reg),
+	url(r"^login/?$",views.login),
+	url(r"^logout/?$",views.logout),
+	url(r"^config/?$",views.config),
+	url(r"^change-password/?$",views.change_password),
+	
+	#diaries
+	url(r"^switch-status/?$",views.sleep-wake),
+	url(r"^list/?(.*)/?(.*)/?$",views.list),
+	url(r"^sleep/(.+)/(.+)/(.+)/?$"),
+	url(r"^analysis/?(.*)/?(.*)/?$",views.analysis),
+	url(r"^$",views.index),
 )
